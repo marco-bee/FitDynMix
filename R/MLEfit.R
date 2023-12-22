441b9027ab0ea19574c43ef2e6b4ca7c1fd0a3d3
marco-bee <marco.bee@unitn.it>
2023-12-22 21:04:19 +0100

    check 22_12a

--- R/MLEfit.R
+++ R/MLEfit.R
@@ -1,32 +1,35 @@
 #' Estimating a dynamic mixture via MLE
 #'
 #' This function fits a dynamic mixture via standard maximum likelihood.
-#' Currently only implemented for the lognormal - generalized Pareto case.
-#' Bootstrap standard errors are computed via parallel computing.
+#' Currently only implemented for the lognormal - generalized Pareto case,
+#' with Cauchy or exponential weight.
 #' @param yObs numerical vector: observed sample.
 #' @param bootreps non-negative integer: number of bootstrap replications. If equal to 0, no standard errors are computed.
 #' @param intTol non-negative scalar: threshold for stopping the computation of the integral in the normalization
 #' constant: if the integral on the interval from n-1 to n is smaller than intTol, the approximation procedure stops.
+#' @param weight 'cau' or 'exp': name of weight distribution.
 #' @return 
 #'
-#' MLEpars (7 x 1) vector: maximum likelihood estimates and
-#' maximized log-likelihood.
+#' MLEpars vector: maximum likelihood estimates and maximized
+#' log-likelihood.
 #'
-#' MLEboot (bootreps x 6) matrix: maximum likelihood estimates obtained in
+#' MLEboot matrix: maximum likelihood estimates obtained in
 #' each bootstrap replication.
 #'
-#' sdMLE (6 x 1) vector: bootstrap standard deviation of the MLEs.
+#' sdMLE vector: bootstrap standard deviation of the MLEs.
 #' 
 #' @details Starting values for mu and sigma are the lognormal MLEs computed
 #' with the observations below the median. Initial values for xi and
 #' tau are the GPD MLEs obtained with the observations above the median.
-#' For the location and scale parameter of the Cauchy, we respectively use the first quartile and log(sd(x)/2).
+#' For the location and scale parameter of the Cauchy, we respectively use
+#' the first quartile and abs(log(sd(x)/2)). For the parameter of the exponential, we use
+#' abs(log(sd(x)/2)).
 #' @keywords dynamic mixture; maximum likelihood.
 #' @seealso [AMLEfit]
 #' @export
 #' @examples
 #' \donttest{
-#' mixFit <- MLEfit(Metro2019,0)}
+#' mixFit <- MLEfit(Metro2019,0,,'cau')}
 #' @references{
 #'   \insertRef{bee22b}{FitDynMix}
 #' }
@@ -34,7 +37,7 @@
 #'
 #' @importFrom Rdpack reprompt
 
-MLEfit <- function(yObs,bootreps,intTol=1e-4)
+MLEfit <- function(yObs,bootreps,intTol=1e-4,weight)
 {
   n = length(yObs)
   mediana = median(yObs)
@@ -44,36 +47,83 @@ MLEfit <- function(yObs,bootreps,intTol=1e-4)
   sigma0 = MASS::fitdistr(y1,'lognormal')$estimate[2]
   xi0 = evir::gpd(y2,mediana)$par.ests['xi']
   beta0 = evir::gpd(y2,mediana)$par.ests['beta']
-  muc0 = quantile(yObs,.5)
-  tau0 = log(sd(yObs)/2)
-  x0Lik = as.numeric(c(muc0,tau0,mu0,sigma0,xi0,beta0))
-  res <- optim(x0Lik,dynloglik, gr=NULL,yObs,intTol,method='L-BFGS-B',lower=c(-Inf,.01,-Inf,.05,10^-10,.1),upper=c(Inf,Inf,Inf,10,Inf,50),control=list(fnscale=-1))
-  estMLE <- c(res$par,res$value) # muc, tau, mu, sigma, xi, beta
   if (bootreps > 0)
   {
-    nreps.list <- sapply(1:bootreps, list)
-    chk <- Sys.getenv("_R_CHECK_LIMIT_CORES_", "")
-    if (nzchar(chk) && chk == "TRUE") {
-      n.cores <- 2L
-    } else {
-      n.cores <- parallel::detectCores()
+    if (weight == 'cau')
+    {	
+      muc0 = quantile(yObs,.5)
+      tau0 = abs(log(sd(yObs)/2))
+      x0Lik = as.numeric(c(muc0,tau0,mu0,sigma0,xi0,beta0))
+      res <- optim(x0Lik,dynloglik, gr=NULL,yObs,intTol,'cau',method='L-BFGS-B',
+                   lower=c(-Inf,.01,-Inf,.01,.01,.01),upper=c(Inf,Inf,Inf,10,Inf,50),control=list(fnscale=-1))
+      estMLE <- c(res$par,res$value) # muc, tau, mu, sigma, xi, beta
+      nreps.list <- sapply(1:bootreps, list)
+      chk <- Sys.getenv("_R_CHECK_LIMIT_CORES_", "")
+      if (nzchar(chk) && chk == "TRUE") {
+        n.cores <- 2L
+      } else {
+        n.cores <- parallel::detectCores()
+      }
+      clust <- parallel::makeCluster(n.cores)
+      MLEboot = matrix(0,bootreps,6)
+      temp <- parallel::parLapply(clust,nreps.list, MLEBoot,yObs,intTol,'cau')
+      parallel::stopCluster(cl=clust)
+      for (i in 1:bootreps)
+      {
+        MLEboot[i,] = as.vector(unlist(temp[[i]]$MLE))
+      }
+      varcov = cov(MLEboot)
+      stddev = sqrt(diag(varcov))
+      out <- list(pars = estMLE, MLEboot = MLEboot, stddev = stddev)
     }
-    clust <- parallel::makeCluster(n.cores)
-    MLEboot = matrix(0,bootreps,6)
-#    temp <- lapply(nreps.list, MLEBoot,yObs,intTol)
-    temp <- parallel::parLapply(clust,nreps.list, MLEBoot,yObs,intTol)
-    parallel::stopCluster(cl=clust)
-    for (i in 1:bootreps)
+    if (weight == 'exp')
     {
-      MLEboot[i,] = as.vector(unlist(temp[[i]]))
+      lambda0 = abs(log(sd(yObs)/2))
+      x0Lik = as.numeric(c(lambda0,mu0,sigma0,xi0,beta0))
+      res <- optim(x0Lik,dynloglik, gr=NULL,yObs,intTol,'exp',method='L-BFGS-B',
+                   lower=c(.01,-Inf,.01,.01,.01),upper=c(Inf,Inf,Inf,Inf,Inf,Inf),control=list(fnscale=-1))
+      estMLE <- c(res$par,res$value) # lambda, mu, sigma, xi, beta
+      nreps.list <- sapply(1:bootreps, list)
+      chk <- Sys.getenv("_R_CHECK_LIMIT_CORES_", "")
+      if (nzchar(chk) && chk == "TRUE") {
+        n.cores <- 2L
+      } else {
+        n.cores <- parallel::detectCores()
+      }
+      clust <- parallel::makeCluster(n.cores)
+      MLEboot = matrix(0,bootreps,5)
+      temp <- parallel::parLapply(clust,nreps.list, MLEBoot,yObs,intTol,'exp')
+      parallel::stopCluster(cl=clust)
+      for (i in 1:bootreps)
+      {
+        MLEboot[i,] = as.vector(unlist(temp[[i]]$MLE))
+      }
+      varcov = cov(MLEboot)
+      stddev = sqrt(diag(varcov))
     }
-    varcov = cov(MLEboot)
-    stddev = sqrt(diag(varcov))
     out <- list(pars = estMLE, MLEboot = MLEboot, stddev = stddev)
-    return(out)
-  }
+  }  
   if (bootreps == 0)
   {
+    if (weight == 'cau')
+    {	
+      muc0 = quantile(yObs,.5)
+      tau0 = abs(log(sd(yObs)/2))
+      x0Lik = as.numeric(c(muc0,tau0,mu0,sigma0,xi0,beta0))
+      res <- optim(x0Lik,dynloglik, gr=NULL,yObs,intTol,'cau',method='L-BFGS-B',
+                   lower=c(-Inf,.01,-Inf,.01,.01,.01),upper=c(Inf,Inf,Inf,Inf,Inf,Inf),control=list(fnscale=-1))
+      estMLE <- c(res$par,res$value) # muc, tau, mu, sigma, xi, beta
+      out <- list(pars = estMLE)
+    }
+    if (weight == 'exp')
+    {	
+      lambda0 = abs(log(sd(yObs)/2))
+      x0Lik = as.numeric(c(lambda0,mu0,sigma0,xi0,beta0))
+      res <- optim(x0Lik,dynloglik, gr=NULL,yObs,intTol,'exp',method='L-BFGS-B',
+                 lower=c(.01,-Inf,.01,.01,.01),upper=c(Inf,Inf,Inf,Inf,Inf,Inf),control=list(fnscale=-1))
+    estMLE <- c(res$par,res$value) # lambda, mu, sigma, xi, beta
     out <- list(pars = estMLE)
+    }
   }
+  return(out)
 }
--- R/dynloglik.R
+++ R/dynloglik.R
@@ -1,40 +1,74 @@
-#' Log-likelihood of a Lognormal-GPD dynamic mixture
+#' Log-likelihood of a Lognormal-GPD dynamic mixture,
+#' with Cauchy or exponential weight.
 #'
 #' This function evaluates the log-likelihood of a Lognormal-GPD dynamic mixture, computing the integral in the normalizing constant via quadrature methods.
 #' @param x (6 by 1) numerical vector: values of CA1, CA2, meanlog, sdlog, xi, beta.
 #' @param y vector: points where the function is evaluated.
 #' @param intTol non-negative scalar: threshold for stopping the computation of the integral in the normalization
 #' constant: if the integral on the interval from n-1 to n is smaller than intTol, the approximation procedure stops.
+#' @param weight 'cau' or 'exp': name of weight distribution.
 #' @return log-likelihood of the lognormal-GPD mixture evaluated at y.
 #' @keywords dynamic mixture.
 #' @export
 #' @examples
 #' x <- c(1,2,0,.5,.25,3.5)
-#' y <- rDynMix(100,x)
-#' fit <- dynloglik(x,y,1e-06)
+#' y <- rDynMix(100,x,'cau')
+#' fit <- dynloglik(x,y,1e-04,'cau')
 
-dynloglik <- function(x,y,intTol)
+dynloglik <- function(x,y,intTol,weight)
 {
-muc <- x[1]
-tau <- x[2]
-mu <- x[3]
-sigma <- x[4]
-xi <- x[5]
-beta <- x[6]
-f <- function(x) (evir::dgpd(x, xi, mu=0, beta)-dlnorm(x,mu,sigma)) * atan((x-muc)/tau)
-p <- pcauchy(y,muc,tau)
-temp <- (1-p) * dlnorm(y,mu,sigma) + p * evir::dgpd(y, xi, mu=0, beta)
-I <- NULL
-I1 <- 10
-i <- 1
-while (abs(I1) > intTol & i <= 2001)
-{
-  temp1 <- pracma::quadinf(f,i-1,i)
-  I1 <- temp1$Q #integral(f,i-1,i)
-  I <- c(I,I1)
-  i <- i + 1
-}
-Z <- 1+(1/pi) * sum(I)
-llik <- sum(log(temp/Z))
+  if (weight == 'cau')
+  {
+    muc <- x[1]
+    tau <- x[2]
+    mu <- x[3]
+    sigma <- x[4]
+    xi <- x[5]
+    beta <- x[6]
+    f <- function(x) (evir::dgpd(x, xi, mu=0, beta)-dlnorm(x,mu,sigma)) * atan((x-muc)/tau)
+    p <- pcauchy(y,muc,tau)
+    temp <- (1-p) * dlnorm(y,mu,sigma) + p * evir::dgpd(y, xi, mu=0, beta)
+    I <- NULL
+    I1 <- 10
+    i <- 1
+    while (abs(I1) > intTol & i <= 2001)
+    {
+      temp1 <- pracma::quadinf(f,i-1,i)
+      I1 <- temp1$Q #integral(f,i-1,i)
+      I <- c(I,I1)
+      i <- i + 1
+    }
+    Z <- 1 + (1/pi) * sum(I)
+    llik <- sum(log(temp/Z))
+  }
+  if (weight == 'exp')
+  {
+    lambda = x[1]
+    mu <- x[2]
+    sigma <- x[3]
+    xi <- x[4]
+    beta <- x[5]
+    f <- function(x) (dlnorm(x,mu,sigma) - evir::dgpd(x, xi, mu=0, beta)) *
+      exp(-lambda*x)
+    p <- pexp(y,lambda)  
+    temp <- (1-p) * dlnorm(y,mu,sigma) + p * evir::dgpd(y, xi, mu=0, beta)
+    I <- NULL
+    I1 <- 10
+    i <- 1
+    while (abs(I1) > intTol & i <= 2001)
+    {
+      temp1 <- pracma::quadinf(f,i-1,i)
+      I1 <- temp1$Q #integral(f,i-1,i)
+      I <- c(I,I1)
+      i <- i + 1
+      if (i > 2000)
+      {
+        print(c(lambda,mu,sigma,xi,beta))
+        print(c(i,I1))
+      }
+    }
+    Z <- 1 + sum(I)
+    llik <- sum(log(temp/Z))
+  }
 return(llik)
 }
